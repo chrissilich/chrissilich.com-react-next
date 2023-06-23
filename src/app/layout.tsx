d35e19654bef26d872aa0afeacb4c9de92afa26f
Christopher Silich <chris.silich@gmail.com>
2023-06-23 23:18:11 +0200

    replace @import google font with link tags to solve editor-styles issue with @import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<link rel="icon" href="/favicon.ico" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>Chris Silich – Creative Technologist Portfolio</title>
+
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,500;0,700;1,300;1,500;1,700&display=swap"
+					rel="stylesheet"
+				/>
 			</head>
 			<body>
 				{children}
